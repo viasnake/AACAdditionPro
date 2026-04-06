90dfeda3faaf69b2f3d2d47e1b5eaa0f9e4f32e4
PhotonDE <photonde@protonmail.com>
2026-04-06 03:39:14 +0200

    Update BrandHider for all Minecraft versions.

--- src/main/java/de/photon/anticheataddition/modules/additions/BrandHider.java
+++ src/main/java/de/photon/anticheataddition/modules/additions/BrandHider.java
@@ -1,82 +1,88 @@
 package de.photon.anticheataddition.modules.additions;
 
+import com.github.retrooper.packetevents.PacketEvents;
+import com.github.retrooper.packetevents.event.PacketListener;
+import com.github.retrooper.packetevents.protocol.packettype.PacketType;
+import com.github.retrooper.packetevents.protocol.player.ClientVersion;
+import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerPluginMessage;
 import de.photon.anticheataddition.AntiCheatAddition;
-import de.photon.anticheataddition.ServerVersion;
 import de.photon.anticheataddition.modules.Module;
 import de.photon.anticheataddition.modules.ModuleLoader;
 import de.photon.anticheataddition.util.execute.Placeholders;
 import de.photon.anticheataddition.util.pluginmessage.ByteBufUtil;
-import de.photon.anticheataddition.util.pluginmessage.MessageChannel;
-import de.photon.anticheataddition.util.reflection.Reflect;
+import de.photon.anticheataddition.util.protocol.PacketAdapterBuilder;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 
-public final class BrandHider extends Module implements Listener
-{
+public final class BrandHider extends Module implements PacketListener {
     public static final BrandHider INSTANCE = new BrandHider();
+
+    private static final String LEGACY_BRAND_CHANNEL = "MC|Brand";
+    private static final String MODERN_BRAND_CHANNEL = "minecraft:brand";
 
     private String brand;
 
-    private BrandHider()
-    {
+    private BrandHider() {
         super("BrandHider");
     }
 
-    public void setBrand(String brand)
-    {
+    public void setBrand(String brand) {
         this.brand = ChatColor.translateAlternateColorCodes('&', brand) + ChatColor.RESET;
         this.updateAllBrands();
     }
 
-    private void updateAllBrands()
-    {
+    private void updateAllBrands() {
         for (Player onlinePlayer : Bukkit.getOnlinePlayers()) updateBrand(onlinePlayer);
     }
 
-    private void updateBrand(final Player player)
-    {
-        final ByteBuf buf = Unpooled.buffer();
-        try
-        {
-            ByteBufUtil.writeString(buf, Placeholders.replacePlaceholders(this.brand, player));
+    private void updateBrand(final Player player) {
+        final String renderedBrand = Placeholders.replacePlaceholders(this.brand, player);
+        final byte[] payload = createBrandPayload(renderedBrand);
+        final String channel = getBrandChannel(player);
 
-            player.sendPluginMessage(AntiCheatAddition.getInstance(), MessageChannel.MC_BRAND_CHANNEL.getChannel().orElseThrow(), ByteBufUtil.toArray(buf));
-        }
-        finally
-        {
+        PacketEvents.getAPI().getPlayerManager().sendPacket(player, new WrapperPlayServerPluginMessage(channel, payload));
+    }
+
+    private static byte[] createBrandPayload(final String brand) {
+        final ByteBuf buf = Unpooled.buffer();
+        try {
+            ByteBufUtil.writeString(buf, brand);
+            return ByteBufUtil.toArray(buf);
+        } finally {
             buf.release();
         }
     }
 
+    private static String getBrandChannel(final Player player) {
+        final ClientVersion clientVersion = PacketEvents.getAPI().getPlayerManager().getClientVersion(player);
+        return clientVersion.isOlderThan(ClientVersion.V_1_13) ? LEGACY_BRAND_CHANNEL : MODERN_BRAND_CHANNEL;
+    }
+
     @Override
-    public void enable()
-    {
-        Bukkit.getMessenger().registerOutgoingPluginChannel(AntiCheatAddition.getInstance(), MessageChannel.MC_BRAND_CHANNEL.getChannel().orElseThrow());
+    public void enable() {
         this.setBrand(loadString(".brand", "Some Spigot"));
 
         final long refreshRate = loadLong(".refresh_rate", 0);
-        if (refreshRate > 0) Bukkit.getScheduler().runTaskTimer(AntiCheatAddition.getInstance(), this::updateAllBrands, 20, refreshRate);
-    }
-
-    @EventHandler
-    public void onJoin(PlayerJoinEvent event)
-    {
-        // Add the mc brand channel to the player's channels.
-        Reflect.fromOBC("entity.CraftPlayer").field("channels").from(event.getPlayer()).asSet(String.class).add(MessageChannel.MC_BRAND_CHANNEL.getChannel().orElseThrow());
-        updateBrand(event.getPlayer());
+        if (refreshRate > 0) {
+            Bukkit.getScheduler().runTaskTimer(AntiCheatAddition.getInstance(), this::updateAllBrands, 20L, refreshRate);
+        }
     }
 
     @Override
-    protected ModuleLoader createModuleLoader()
-    {
+    protected ModuleLoader createModuleLoader() {
         return ModuleLoader.builder(this)
-                           .setAllowedServerVersions(ServerVersion.MC119.getSupVersionsTo())
-                           .build();
+                .addPacketListeners(PacketAdapterBuilder.of(this, PacketType.Play.Server.PLUGIN_MESSAGE).onSending((event, user) -> {
+                    final WrapperPlayServerPluginMessage packet = new WrapperPlayServerPluginMessage(event);
+                    final String channel = packet.getChannelName();
+
+                    if (LEGACY_BRAND_CHANNEL.equals(channel) || MODERN_BRAND_CHANNEL.equals(channel)) {
+                        final String renderedBrand = Placeholders.replacePlaceholders(this.brand, user.getPlayer());
+                        packet.setData(createBrandPayload(renderedBrand));
+                        event.markForReEncode(true);
+                    }
+                }).build()).build();
     }
 }
